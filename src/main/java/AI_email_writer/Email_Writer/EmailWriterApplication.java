ef5304540cacdafc39930cbf3c6d3185e75cb222
G Akash <gakash@Gs-MacBook-Air.local>
2026-04-12 11:55:25 +0530

    Initial-Commit

---
+++ src/main/java/AI_email_writer/Email_Writer/EmailWriterApplication.java
@@ -0,0 +1,13 @@
+package AI_email_writer.Email_Writer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailWriterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailWriterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AI_email_writer/Email_Writer/EmailWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package AI_email_writer.Email_Writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
